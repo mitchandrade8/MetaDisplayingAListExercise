9829537c13d6b7db86c95ee68d61125dac98c379
mitchandrade8 <54218379+mitchandrade8@users.noreply.github.com>
2023-08-30 12:45:12 -0600

    Initial Commit

---
+++ MetaDisplayingAListExercise/MetaDisplayingAListExerciseApp.swift
@@ -0,0 +1,17 @@
+//
+//  MetaDisplayingAListExerciseApp.swift
+//  MetaDisplayingAListExercise
+//
+//  Created by Mitch Andrade on 8/30/23.
+//
+
+import SwiftUI
+
+@main
+struct MetaDisplayingAListExerciseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
